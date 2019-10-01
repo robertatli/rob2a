07d46e44395b0f343222007eb841caaa1d18f35e
sigmarola <42818933+sigmarola@users.noreply.github.com>
2019-10-01 09:00:44 +0000

    Update verk2v2.c

--- verkefni2/verk2v2.c
+++ verkefni2/verk2v2.c
@@ -17,8 +17,6 @@
 #pragma config(Motor,  port10,          rightMotor,    tmotorVex393, openLoop, reversed)
 
 const int FULL_POWER = 45;
-const int TURN_POWER = 40;
-const int BASE_TIME = 700;
 // reiknar ut snuninga a hjoli
 float diameterOfWheel = 4.133858; //inches
 float circumference = diameterOfWheel*PI; //inches
@@ -29,15 +27,15 @@ void resetSensorValue(){
   SensorValue[leftEncoder] = 0;
 }
 void stopMotor(){
-	wait1Msec(1000);
 	motor[rightMotor] = 0;
 	motor[leftMotor] = 0;
+	wait1Msec(1000);
 }
 
 void drivedist(bool b_f, int dist){
 	resetSensorValue();
 	int dir = (b_f)? (1):(-1);
-	while(SensorValue[leftEncoder] < dist)  // While less than 50 cm
+	while(abs(SensorValue[leftEncoder]) < dist)  // While less than 50 cm
   		{
 				motor[rightMotor] = FULL_POWER*dir;
 				motor[leftMotor] = FULL_POWER*dir;
@@ -57,66 +55,11 @@ task main()
 	int wheelTurns = rotations*360;
 
 
-	for(int i = 1; i<6;i++){
+	for(int i = 1; i<5;i++){
 		drivedist(true,wheelTurns);
-		stopMotor();
+		wait10Msec(1);
 		drivedist(false,wheelTurns);
-		wheelTurns += wheelTurns
+		wheelTurns += wheelTurns;
+		//printf(wheelTurns);
 	}
-
-
-
-/*
-			bool direction = true;
-			if(i % 2 == 0) direction = false;
-				else direction = true;*/
-
-	/*for(int i=1;i<6;i++){
-	drive(BASE_TIME*i,true);
-	drive(BASE_TIME*i,false);
-	}*/
-
-}
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-
-
-
-
-/*
-task display(){
-		bLCDBacklight = true;									// Turn on LCD Backlight
-	string mainBattery, backupBattery;
-	while(true)														// An infinite loop to keep the program running until you terminate it
-	{
-		clearLCDLine(0);											// Clear line 1 (0) of the LCD
-		clearLCDLine(1);											// Clear line 2 (1) of the LCD
-		//Display the Primary Robot battery voltage
-		displayLCDString(0, 0, "Primary: ");
-		sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V'); //Build the value to be displayed
-		displayNextLCDString(mainBattery);
-		//Display the Backup battery voltage
-		displayLCDString(1, 0, "Backup: ");
-		sprintf(backupBattery, "%1.2f%c", BackupBatteryLevel/1000.0, 'V');	//Build the value to be displayed
-		displayNextLCDString(backupBattery);
-		//Short delay for the LCD refresh rate
-		wait1Msec(100);
-	}
-}*/
-/*
-void drive(int drivetime, bool b_f){
-	int dir = (b_f)? (1):(-1);
-	motor[rightMotor] = FULL_POWER*dir;
-	motor[leftMotor]  = FULL_POWER*dir;
-	wait1Msec(drivetime);
-}*/
-
-
-
-
-
-
-
-
 
