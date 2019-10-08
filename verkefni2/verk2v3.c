573bcbcfd962e557d70dd9d15b98be0e21c8470e
robertatli <robbster10@gmail.com>
2019-10-08 08:57:18 +0000

    removed commented code

--- verkefni2/verk2v3.c
+++ verkefni2/verk2v3.c
@@ -21,34 +21,6 @@ const int TURN_POWER = 60;
 const int BASE_TIME = 700;
 bool b_f = true;
 
-/*
-task display(){
-		bLCDBacklight = true;									// Turn on LCD Backlight
-	string mainBattery, backupBattery;
-	while(true)														// An infinite loop to keep the program running until you terminate it
-	{
-		clearLCDLine(0);											// Clear line 1 (0) of the LCD
-		clearLCDLine(1);											// Clear line 2 (1) of the LCD
-		//Display the Primary Robot battery voltage
-		displayLCDString(0, 0, "Primary: ");
-		sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V'); //Build the value to be displayed
-		displayNextLCDString(mainBattery);
-		//Display the Backup battery voltage
-		displayLCDString(1, 0, "Backup: ");
-		sprintf(backupBattery, "%1.2f%c", BackupBatteryLevel/1000.0, 'V');	//Build the value to be displayed
-		displayNextLCDString(backupBattery);
-		//Short delay for the LCD refresh rate
-		wait1Msec(100);
-	}
-}*/
-/*
-void drive(int drivetime, bool b_f){
-	int dir = (b_f)? (1):(-1);
-	motor[rightMotor] = FULL_POWER*dir;
-	motor[leftMotor]  = FULL_POWER*dir;
-	wait1Msec(drivetime);
-}*/
-
 void resetSensorValue(){
 	SensorValue[rightEncoder] = 0;
   SensorValue[leftEncoder] = 0;
@@ -117,11 +89,6 @@ task main()
 			driveturn(BASE_TIME,true,turnLeft, degreesToTurn);
 	}
 
-
-	/*for(int i=1;i<6;i++){
-	drive(BASE_TIME*i,true);
-	drive(BASE_TIME*i,false);
-	}*/
 
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
