837408c9161048d80445c25cd815ff8c5593b9a9
sigmarola <42818933+sigmarola@users.noreply.github.com>
2019-11-12 09:56:30 +0000

    Add files via upload

---
+++ verkefni4/verk4/main.c
@@ -0,0 +1,26 @@
+
+#pragma config(Sensor, in2,    centerLineFollower, sensorLineFollower)
+#pragma config(Sensor, in3,    rightLineFollower, sensorLineFollower)
+#pragma config(Sensor, in4,    potentiometer,  sensorPotentiometer)
+#pragma config(Sensor, in5,    lightSensor,    sensorReflection)
+#pragma config(Sensor, in6,    gyro,           sensorGyro)
+#pragma config(Sensor, in7,    accelerometer,  sensorAccelerometer)
+#pragma config(Sensor, dgtl1,  rightEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  leftEncoder,    sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  armTouchSensor, sensorTouch)
+#pragma config(Sensor, dgtl6,  touchSensor,    sensorTouch)
+#pragma config(Sensor, dgtl9,  sonarSensor,    sensorSONAR_cm)
+#pragma config(Sensor, dgtl11, armEncoder,     sensorQuadEncoder)
+#pragma config(Motor,  port1,           leftMotor,     tmotorVex393, openLoop)
+#pragma config(Motor,  port6,           clawMotor,     tmotorVex393, openLoop)
+#pragma config(Motor,  port7,           armMotor,      tmotorVex393, openLoop)
+#pragma config(Motor,  port10,          rightMotor,    tmotorVex393, openLoop, reversed)
+
+#include "../verk4/tasks.c"
+
+task main()
+{
+StartTask(drive);
+StartTask(stopTasks);
+while(1==1){}
+}
---
+++ verkefni4/verk4/tasks.c
@@ -0,0 +1,13 @@
+
+task drive();
+task stopTasks();
+
+task drive(){
+	//code
+}
+task stopTasks(){
+		while(1==1){
+	if(vexRT[Btn7U]==1 || touchSensor == 1){
+		StopAllTasks();
+	}}
+}
