6e0e3736bb0ffe18e35d8c07f5ebba2f43c22f6f
robertatli <robbster10@gmail.com>
2019-11-12 10:20:54 +0000

    Update main.c

--- verkefni4/verk4/main.c
+++ verkefni4/verk4/main.c
@@ -1,4 +1,5 @@
 
+#pragma config(Sensor, in1,    leftLineFollower, sensorLineFollower)
 #pragma config(Sensor, in2,    centerLineFollower, sensorLineFollower)
 #pragma config(Sensor, in3,    rightLineFollower, sensorLineFollower)
 #pragma config(Sensor, in4,    potentiometer,  sensorPotentiometer)
@@ -9,8 +10,7 @@
 #pragma config(Sensor, dgtl3,  leftEncoder,    sensorQuadEncoder)
 #pragma config(Sensor, dgtl5,  armTouchSensor, sensorTouch)
 #pragma config(Sensor, dgtl6,  touchSensor,    sensorTouch)
-#pragma config(Sensor, dgtl9,  sonarSensor,    sensorSONAR_cm)
-#pragma config(Sensor, dgtl11, armEncoder,     sensorQuadEncoder)
+#pragma config(Sensor, dgtl10, sonarSensor,    sensorSONAR_cm)
 #pragma config(Motor,  port1,           leftMotor,     tmotorVex393, openLoop)
 #pragma config(Motor,  port6,           clawMotor,     tmotorVex393, openLoop)
 #pragma config(Motor,  port7,           armMotor,      tmotorVex393, openLoop)
