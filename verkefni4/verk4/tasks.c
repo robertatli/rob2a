abceec6f4b19df9e28b3cf77ba5a86c089b6c726
robertatli <robbster10@gmail.com>
2019-11-12 10:20:11 +0000

    Update tasks.c

--- verkefni4/verk4/tasks.c
+++ verkefni4/verk4/tasks.c
@@ -3,7 +3,29 @@ task drive();
 task stopTasks();
 
 task drive(){
-	//code
+	wait1Msec(1000);						// Robot waits for 2000 milliseconds before executing program
+	while(1==1){
+		if(SensorValue(lightSensor) > 650){
+			//while(SensorValue(lightSensor) > 650){
+				motor[rightMotor] = 0;
+				motor[leftMotor] = 0;
+			//}	
+		}
+		if(SensorValue(lightSensor) < 650){
+			while(SensorValue(lightSensor) < 650) // While the ambient lightSensor reads a value lesser than 200
+			{
+					if(SensorValue(sonarSensor) > 35 || SensorValue(sonarSensor) == -1){
+						motor[rightMotor] = 83;
+						motor[leftMotor] = 	83;
+					}
+					else{
+					motor[rightMotor] = 83;			// Motor on port2 is run at half (63) power forward
+					motor[leftMotor]  = -83;			// Motor on port3 is run at half (63) power forward
+					wait1Msec(500);
+					}
+			}
+		}
+	}
 }
 task stopTasks(){
 		while(1==1){
